7cafa596c4a1a9f4c393f0754332c3e03ab210e4
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2025-09-10 12:09:15 +0700

    Pemrograman Dart - Bagian 2

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,22 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+// Tugas Praktikum Praktikum
+void main() {
+  String nama = "Billy Maulana Ferdinan";
+  String nim = "2341720196";
+
+  for (int angka = 0; angka <= 201; angka++) {
+    if (isPrima(angka)) {
+      print("$angka adalah bilangan prima - $nama ($nim)");
+    }
+  }
 }
+
+bool isPrima(int n) {
+  if (n < 2) return false; // Bilangan < 2 bukan prima
+  for (int i = 2; i <= n ~/ 2; i++) {
+    if (n % i == 0) return false; // Jika habis dibagi, bukan prima
+  }
+  return true;
+}
+
